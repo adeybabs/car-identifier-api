1eb763824841941aed6704de0487a7f0850e909e
Adeybabs <37551521+adeybabs@users.noreply.github.com>
2021-11-02 12:00:57 +0100

    added exception handling

---
+++ Car-Identifier/src/main/java/com/simple/Car/Identifier/exception/IllegalApiParamException.java
@@ -0,0 +1,8 @@
+package com.simple.Car.Identifier.exception;
+
+public class IllegalApiParamException extends RuntimeException {
+
+    public IllegalApiParamException(String s) {
+        super(s);
+    }
+}
